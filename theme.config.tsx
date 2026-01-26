6d8baa1b8c34cd39ea5ea0a5565f2dc573abff65
mr.r <62205964+MrR812@users.noreply.github.com>
2026-01-26 10:33:01 +0700

    fix theme config - add missing export

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,23 +1,66 @@
-import React from 'react'; 
-import { DocsThemeConfig, useConfig } from 'nextra-theme-docs'; 
-import { useRouter } from 'next/router'; 
-import Search from '@components/Search'; 
- 
-function useHead() { 
-  const { asPath } = useRouter(); 
-  const { frontMatter, title } = useConfig(); 
-  const url = `https://pratamalabs.github.io${asPath}`; 
-  const description = frontMatter.description || "Dokumentasi Pratama Labs"; 
- 
-  return ( 
-    <> 
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" /> 
-      <link rel="icon" type="image/x-icon" href="/static/ox.ico" /> 
-      <meta httpEquiv="Content-Language" content="en" /> 
-      <meta name="description" content={description} /> 
-      <meta name="og:title" content={title} /> 
-      <meta name="og:description" content={description} /> 
-      <meta name="og:url" content={url} /> 
-    </> 
-  ); 
-} 
+import React from 'react';
+import { DocsThemeConfig, useConfig } from 'nextra-theme-docs';
+import { useRouter } from 'next/router';
+import Search from '@components/Search';
+
+function useHead() {
+  const { asPath } = useRouter();
+  const { frontMatter, title } = useConfig();
+  const url = `https://pratamalabs.github.io${asPath}`;
+  const description = frontMatter.description || "Dokumentasi Pratama Labs";
+
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" type="image/x-icon" href="/static/ox.ico" />
+      <meta httpEquiv="Content-Language" content="en" />
+      <meta name="description" content={description} />
+      <meta name="og:title" content={title} />
+      <meta name="og:description" content={description} />
+      <meta name="og:url" content={url} />
+    </>
+  );
+}
+
+function useNextSeoProps() {
+  const { asPath } = useRouter();
+  const arr = asPath.replace(/[-_]/g, ' ').split('/');
+  const category = (arr[1][0] !== '#' && arr[1]) || 'Pratama Labs';
+  const rawTitle = arr[arr.length - 1];
+  const title = /[a-z]/.test(rawTitle) && /[A-Z]/.test(rawTitle) ? rawTitle : '%s';
+
+  return {
+    titleTemplate: `${title} - ${
+      rawTitle === category ? 'Documentation' : category.replace(/(^\w|\s\w)/g, (m) => m.toUpperCase())
+    }`,
+  };
+}
+
+const config: DocsThemeConfig = {
+  logo: (
+    <div style={{ fontWeight: 550, fontSize: '18px' }}>
+      🚀 Pratama Labs
+    </div>
+  ),
+  project: {
+    link: 'https://github.com/pratamalabs/pratamalabs.github.io',
+  },
+  docsRepositoryBase: 'https://github.com/pratamalabs/pratamalabs.github.io/blob/main',
+  footer: {
+    text: 'Pratama Labs © 2026',
+  },
+  search: {
+    component: <Search />,
+  },
+  head: useHead,
+  primaryHue: { dark: 200, light: 200 },
+  sidebar: {
+    defaultMenuCollapseLevel: 1,
+  },
+  toc: {
+    backToTop: true,
+  },
+  useNextSeoProps: useNextSeoProps,
+};
+
+export default config;
