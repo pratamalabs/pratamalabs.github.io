67e34dc863420f188a6c1c40dec2b8d8f6328240
mr.r <62205964+MrR812@users.noreply.github.com>
2026-01-26 10:25:46 +0700

    update site url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://overextended.dev/',
+  siteUrl: 'https://pratamalabs.github.io/',
   generateRobotsTxt: true,
   generateIndexSitemap: false,
   outDir: './out',
